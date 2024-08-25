778c8d453c0972e9bbb9fa3a4158d1f52e6bba7d
NooranTarek <noort.mohamed@gmail.com>
2024-08-25 12:30:58 +0300

    updating author

--- controllers/author.controller.js
+++ controllers/author.controller.js
@@ -14,7 +14,18 @@ const addAuthor=catchAsyncErr(async (req,res)=>{
     res.status(201).json({ message: "Author Addedd successfully",author});
 
 })
+const updateAuthor = catchAsyncErr(async (req, res) => {
+    const id = req.params.id; 
+    const { name, email, bio } = req.body; 
+    const author = await Author.findByIdAndUpdate(id, { name, email, bio }, { new: true });
+    if (!author) {
+        return res.status(404).json({ message: "Author not found" });
+    }
+  res.status(200).json({ message: "Author updated successfully", author });
+});
+
 
 export{
-    addAuthor
+    addAuthor,
+    updateAuthor
 }
--- routes/author.route.js
+++ routes/author.route.js
@@ -3,4 +3,6 @@ import * as authorController from '../controllers/author.controller.js'
 
 const authorRouter=express.Router();
 authorRouter.post('/',authorController.addAuthor);
+authorRouter.put('/:id',authorController.updateAuthor);
+
 export default authorRouter;
